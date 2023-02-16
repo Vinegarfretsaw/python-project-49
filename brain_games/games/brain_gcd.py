07c6a1fde6d971895ff29024d1cee0e99ae3a3a3
family <family-laptop@MacBook-Air-family.local>
2023-02-16 11:47:12 +0800

    fix brain-gcd game

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 GAME = "Find the greatest common divisor of given numbers."
@@ -8,22 +9,5 @@ def play():
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     question = f'{num1} {num2}'
-    return question, gcd(num1, num2)
-
-
-def gcd(num1, num2):
-    same_divider = []
-    divider1 = []
-    divider2 = []
-    for i in range(1, num1):
-        if num1 % i == 0:
-            divider1.append(i)
-    for i in range(1, num2):
-        if num2 % i == 0:
-            divider2.append(i)
-    for i in divider1:
-        for j in divider2:
-            if i == j:
-                same_divider.append(i)
-                right_answer = max(same_divider)
-    return right_answer
+    right_answer = math.gcd(num1, num2)
+    return question, right_answer
