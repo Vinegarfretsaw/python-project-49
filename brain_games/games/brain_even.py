6d09fb883483829cc3df3d3b7f4097030b59e0ca
family <family-laptop@MacBook-Air-family.local>
2023-02-17 08:34:18 +0800

    fix some things, part two

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-GAME = 'Answer "yes" if the number is even, otherwise answer "no".' 
+GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def play():
