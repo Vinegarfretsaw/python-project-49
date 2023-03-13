5e9c14002878e900d3069613f6bc1fe290d04f2a
family <family-laptop@MacBook-Air-family.local>
2023-03-13 16:48:54 +0200

    some fixes3

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -14,16 +14,15 @@ def get_information():
 
 
 def calculate(num1, num2, operation):
-    while True:
-        if operation == '0':
-            break
-        if operation not in ['+', '-', '*']:
-            continue
-        else:
-            if operation == '+':
-                right_answer = num1 + num2
-            elif operation == '-':
-                right_answer = num1 - num2
-            elif operation == '*':
-                right_answer = num1 * num2
-            return str(right_answer)
+    if operation == '0':
+        return ''
+    if operation not in ['+', '-', '*']:
+        return ''
+    else:
+        if operation == '+':
+            right_answer = num1 + num2
+        elif operation == '-':
+            right_answer = num1 - num2
+        elif operation == '*':
+            right_answer = num1 * num2
+        return str(right_answer)
